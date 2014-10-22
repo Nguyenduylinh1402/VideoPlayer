a2d70235cb8a93ec402b97c66a409f5137f4dad1
Nguyen Duy Linh <linh.duy.ng@gmail.com>
2014-10-22 21:38:22 +0700

    create

---
+++ src/com/linhnguyen/videoplayer/VideoPlayer.java
@@ -0,0 +1,16 @@
+package com.linhnguyen.videoplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class VideoPlayer extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_video_player);
+	}
+
+}
