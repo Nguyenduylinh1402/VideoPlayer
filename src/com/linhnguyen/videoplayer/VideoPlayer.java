a8b078aade4ab8f07b361b8332f629c87f4dfcbb
unknown <linhduyng01@gmail.com>
2014-10-24 01:57:01 +0700

    simple code

--- src/com/linhnguyen/videoplayer/VideoPlayer.java
+++ src/com/linhnguyen/videoplayer/VideoPlayer.java
@@ -1,9 +1,10 @@
 package com.linhnguyen.videoplayer;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class VideoPlayer extends Activity {
 
@@ -11,6 +12,13 @@ public class VideoPlayer extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_video_player);
+		
+		VideoView vid = (VideoView)findViewById(R.id.videoView);
+		//vid.setVideoPath("android.resource://com.linhnguyen.videoplayer"+R.raw.prime);
+		vid.setVideoPath("/storage/emulated/0/Download/Prime.mp4");
+		vid.setMediaController(new MediaController(this));
+		vid.start();
+		vid.requestFocus();
 	}
 
 }
